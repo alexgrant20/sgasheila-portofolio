873eac7169d7d5fa446d1075ebae014793532e6f
Steven Alexander Grant <alexgrant200@yahoo.com>
2026-08-31 14:03:23 +0700

    remove .next in the build

--- .next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"e0u/vFB8wjalMevNqpdfYUBTthLec3Qm1fHgQC2iyF0=\"\n}"
--- .next/dev/static/chunks/_219uq1s._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/[root-of-the-server]__0v8k-a9._.css","static/chunks/node_modules_next_dist_20wefz_._.js","static/chunks/src_0t-ccn4._.js","static/chunks/node_modules_06ypr7d._.js"],
-    source: "entry"
-});
